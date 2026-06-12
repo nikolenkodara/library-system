a8d7b2af92d66ecc47db807082ab122621ef4313
dashyk0 <dasapusina111@gmail.com>
2026-06-12 22:16:38 +0300

    Add port configuration to database connection

--- utils.py
+++ utils.py
@@ -11,6 +11,7 @@ def get_db_connection():
     try:
         conn = mysql.connector.connect(
             host=current_app.config['MYSQL_HOST'],
+            port=current_app.config['MYSQL_PORT'],      # добавлено
             user=current_app.config['MYSQL_USER'],
             password=current_app.config['MYSQL_PASSWORD'],
             database=current_app.config['MYSQL_DATABASE']
