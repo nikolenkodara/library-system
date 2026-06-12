25d26e4228ef25fac3153cf0d3f00fe9a94cae08
dashyk0 <dasapusina111@gmail.com>
2026-06-12 22:39:41 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,11 +2,11 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'default-dev-key')
-    MYSQL_HOST = os.environ.get('MYSQL_HOST', 'localhost')
-    MYSQL_PORT = int(os.environ.get('MYSQL_PORT', 3306))   # добавлено
-    MYSQL_USER = os.environ.get('MYSQL_USER', 'root')
-    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', '')
-    MYSQL_DATABASE = os.environ.get('MYSQL_DATABASE', 'library_db')
+    MYSQL_HOST = os.environ.get('MYSQLHOST', 'localhost')
+    MYSQL_PORT = int(os.environ.get('MYSQLPORT', 3306))
+    MYSQL_USER = os.environ.get('MYSQLUSER', 'root')
+    MYSQL_PASSWORD = os.environ.get('MYSQLPASSWORD', '')
+    MYSQL_DATABASE = os.environ.get('MYSQLDATABASE', 'library_db')
     UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static', 'uploads')
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024
     ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
